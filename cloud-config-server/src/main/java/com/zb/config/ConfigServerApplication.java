16df8ffd0152e2c83c904a7db0320cbdc9476d69
itzhangnima <itzhangnima@163.com>
2018-03-07 22:04:21 +0800

    config-server仓库

---
+++ cloud-config-server/src/main/java/com/zb/config/ConfigServerApplication.java
@@ -0,0 +1,35 @@
+package com.zb.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 2018/3/7 21:57
+ **
+        * 通过@EnableConfigServer注解激活配置服务.
+* 说明：
+        * 在application.yml中有个git.uri的配置，⽬前配置的是https://githu
+        b.com/eacdy/spring-cloud-study/
+        * 获取git上的资源信息遵循如下规则：
+        * /{application}/{profile}[/{label}]
+        * /{application}-{profile}.yml
+        * /{label}/{application}-{profile}.yml
+        * /{application}-{profile}.properties
+        * /{label}/{application}-{profile}.properties
+        *
+        * 例如本例：可使⽤以下路径来访问microservice-config-client-dev.pr
+        operties：
+        * http://localhost:8040/microservice-config-client-dev.prope
+        rties
+        * http://localhost:8040/microservice-config-client/dev
+ * @author zb
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}

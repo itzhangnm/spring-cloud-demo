0be2137742118930acd0f17474f00d4cc8714e16
itzhangnima <itzhangnima@163.com>
2018-03-07 22:35:27 +0800

    config-client测试

---
+++ cloud-config-client/src/main/java/com/zb/config/client/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.zb.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 2018/3/7 22:17
+ *
+ * @author zb
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class,args);
+    }
+}
